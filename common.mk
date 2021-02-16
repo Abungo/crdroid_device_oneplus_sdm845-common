a45ca87ceb596d421267c58d192b25a7053e727e
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    sdm845-common: Migrate to libperfmg power HAL AIDL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- common.mk
+++ common.mk
@@ -301,8 +301,8 @@ PRODUCT_BOOT_JARS += \
     oneplus-fwk
 
 # Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+$(call inherit-product, hardware/oneplus/libqti-perfd-client/libqti-perfd-client.mk)
+$(call inherit-product, hardware/oneplus/power-libperfmgr/power-libperfmgr.mk)
 
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
